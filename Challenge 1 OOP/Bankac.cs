bc1ff128418411c649a54f977b4aa5e606864dfd
Mary <mary.vatanparast@gmail.com>
2021-02-03 12:49:50 +0100

    Add project files.

---
+++ Challenge 1 OOP/Bankac.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1_OOP
+{
+    class Bankac
+    {
+        public string AcNumber;
+        public double Balance;
+        public string CustomerName;
+        public string Email;
+        public int PhoneNumber;
+
+        public Bankac(string Pacnumber, double PBalance, string PCusName, string PEmail, int Pphonenum)
+        {
+            AcNumber = Pacnumber;
+            Balance = PBalance;
+            CustomerName = PCusName;
+            Email = PEmail;
+            PhoneNumber = Pphonenum;
+
+
+        }
+
+        public string AddMoney(double PAddmoney)
+        {
+            Balance = Balance + PAddmoney;
+           return $"Your New Balance is:{Balance}";
+        }
+
+        public string DeductMoney(double PDeductmoney)
+        {
+            if (PDeductmoney <= Balance)
+            {
+                Balance = Balance - PDeductmoney;
+                return $"Your New Balance is:{Balance}";
+            }
+            else
+            {
+                return "Error";
+            }
+            
+        }
+
+
+
+    }
+}
---
+++ Challenge 1 OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            Bankac Customer1 = new Bankac("123456", 1500, "John Robert", "John@gmail.com", 432542) ;
+
+            Console.WriteLine(Customer1.CustomerName);
+            Console.WriteLine(Customer1.Balance);
+            Console.WriteLine(Customer1.Email);
+            Console.WriteLine(Customer1.AcNumber);
+            Console.WriteLine(Customer1.PhoneNumber);
+            Console.WriteLine("***************************************************************************************");
+            Console.WriteLine(Customer1.AddMoney(150));
+            Console.WriteLine("***************************************************************************************");
+            Console.WriteLine(Customer1.DeductMoney(1651));
+           
+           
+         
+        }
+    }
+}
